e59c7ae8c4e899e9614b79c78403a016afe116a3
github_piyush2108 <piyusharmap@gmail.com>
2023-07-04 21:21:21 +0530

    Changes with api

--- src/constant.js
+++ src/constant.js
@@ -1,11 +1,11 @@
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RESTAURANTS_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.2876525&lng=75.84758529999999&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.2876525&lng=75.84758529999999&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANTS_MENU_API_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.2876525&lng=75.84758529999999&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.2876525&lng=75.84758529999999&restaurantId=";
 
 export const BODY_SHIMMER_VALUE = 6;
 
